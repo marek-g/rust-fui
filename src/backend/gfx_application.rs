d5c65f2b60d9d1cb1f278caf6b67b9d087f2175f
mgibek <marek@mail.com>
2017-01-22 21:45:32 -0500

    Started implementing primitive drawing.

--- src/backend/gfx_application.rs
+++ src/backend/gfx_application.rs
@@ -3,11 +3,14 @@ extern crate gfx_core;
 extern crate gfx_window_dxgi;
 extern crate gfx_device_dx11;
 extern crate winit;
+extern crate nalgebra;
 
 use self::gfx_core::Factory;
 use self::gfx_core::Device;
 use gfx::traits::FactoryExt;
-use ::render::primitive::Primitive;
+use ::common::color::Color;
+use ::render::primitive::{ Primitive, PrimitiveKind };
+use std::ops::Mul;
 
 pub type ColorFormat = gfx::format::Rgba8;
 pub type DepthFormat = gfx::format::DepthStencil;
@@ -31,6 +34,10 @@ pub struct GFXApplication {
     target_view: gfx_core::handle::RenderTargetView<gfx_device_dx11::Resources, ColorFormat>,
     pipeline: gfx::PipelineState<gfx_device_dx11::Resources, pipe::Meta>,
     encoder: gfx::Encoder<gfx_device_dx11::Resources, gfx_device_dx11::CommandBuffer<gfx_device_dx11::CommandList>>,
+
+    view_matrix: nalgebra::Matrix3<f32>,
+
+    rotate: f64,
 }
 
 impl GFXApplication {
@@ -55,13 +62,18 @@ impl GFXApplication {
 
         let mut encoder: gfx::Encoder<gfx_device_dx11::Resources, gfx_device_dx11::CommandBuffer<gfx_device_dx11::CommandList>>  = factory.create_command_buffer().into();
 
-        GFXApplication {
-            window: window,
+        let width = window.size.0 as f32;
+        let height = window.size.1 as f32;
+
+        GFXApplication {window: window,
             device: device,
             factory: factory,
             target_view: target_view,
             pipeline: pipeline,
             encoder: encoder,
+
+            view_matrix: GFXApplication::view_matrix_from_resolution(width, height),
+            rotate: 0.0,
         }
     }
 
@@ -79,10 +91,76 @@ impl GFXApplication {
 
     pub fn draw_primitives(&mut self, primitives: Vec<Primitive>,
                            width: f32, height: f32) {
-        const TRIANGLE: [Vertex; 3] = [
-            Vertex { pos: [ -0.5, -0.5 ], color: [1.0, 1.0, 1.0] },
-            Vertex { pos: [  0.5, -0.5 ], color: [0.0, 1.0, 0.0] },
-            Vertex { pos: [  0.0,  0.5 ], color: [0.0, 0.0, 0.0] }
+        self.rotate += 0.1;
+        let rotate = self.rotate;
+        //let (x, y) = ((args.width / 2) as f64,
+        //              (args.height / 2) as f64);
+
+        //let glyph_cache = &mut self.glyph_cache;
+
+        // Clear the screen.
+        self.encoder.clear(&self.target_view, [0.1, 0.2, 0.3, 1.0]);
+
+        for primitive in &primitives {
+            match primitive.kind {
+
+                PrimitiveKind::Line { ref color, thickness, x1, y1, x2, y2 } => {
+                    self.line([color[1], color[2], color[3], color[0]],
+                        thickness / 2.0f32,
+                        [x1, y1, x2, y2],
+                        nalgebra::new_identity(3));
+                        //context.transform.trans(400.0,200.0).rot_rad(rotate).trans(-200.0,-100.0));
+                },
+
+                PrimitiveKind::Rectangle { ref color, x, y, width, height } => {
+                    /*self.rectangle([color[1], color[2], color[3], color[0]],
+                              [x, y, width, height],
+                              nalgebra::new_identity(2));*/
+                    let matrix = self.view_matrix.clone();
+                    self.rectangle([color[1], color[2], color[3], color[0]],
+                                   [x, y, x + width - 1.0f32, y + height - 1.0f32],
+                                   matrix);
+                                   //nalgebra::new_identity(3));
+                              //context.transform.trans(400.0,200.0).rot_rad(rotate).trans(-200.0,-100.0));
+                },
+
+                PrimitiveKind::Text { ref color, x, y, size, text: ref src_text } => {
+                    self.text([color[1], color[2], color[3], color[0]],
+                        size as u32,
+                        src_text,
+                        //glyph_cache,
+                         nalgebra::new_identity(3));
+                        //context.transform.trans(400.0,200.0).rot_rad(rotate).trans(x as f64, (y + size) as f64).trans(-200.0,-100.0));
+                }
+            }
+        }
+
+        self.encoder.flush(&mut self.device);
+        self.window.swap_buffers(1);
+        self.device.cleanup();
+    }
+
+    pub fn text_width(&mut self, size: f32, text: &str) -> f32 {
+        //self.glyph_cache.width(size as FontSize, &text) as f32
+        120.0
+    }
+
+    fn line(&mut self, color: Color, thickness: f32, points: [f32; 4], matrix: nalgebra::Matrix3<f32>) {
+
+    }
+
+    fn rectangle(&mut self, color: Color, points: [f32; 4], matrix: nalgebra::Matrix3<f32>) {
+        let p1 = matrix.mul(nalgebra::Point3::new(points[0], points[1], 1.0f32));
+        let p2 = matrix.mul(nalgebra::Point3::new(points[2], points[3], 1.0f32));
+        let col = [color[1], color[2], color[3]];
+
+        let TRIANGLE: [Vertex; 6] = [
+            Vertex { pos: [ p1[0], p1[1] ], color: col },
+            Vertex { pos: [ p2[0], p1[1] ], color: col },
+            Vertex { pos: [ p1[0], p2[1] ], color: col },
+            Vertex { pos: [ p2[0], p1[1] ], color: col },
+            Vertex { pos: [ p2[0], p2[1] ], color: col },
+            Vertex { pos: [ p1[0], p2[1] ], color: col },
         ];
         let (vertex_buffer, slice) = self.factory.create_vertex_buffer_with_slice(&TRIANGLE, ());
 
@@ -91,58 +169,16 @@ impl GFXApplication {
             out: self.target_view.clone()
         };
 
-        self.encoder.clear(&data.out, [0.1, 0.2, 0.3, 1.0]);
         self.encoder.draw(&slice, &self.pipeline, &data);
-        self.encoder.flush(&mut self.device);
-        self.window.swap_buffers(1);
-        self.device.cleanup();
-
-
-        /*use self::graphics::*;
-
-        self.rotate += 0.1;
-        let rotate = self.rotate;
-        //let (x, y) = ((args.width / 2) as f64,
-        //              (args.height / 2) as f64);
-
-        let gl = &mut self.gl;
-        let glyph_cache = &mut self.glyph_cache;
-
-        gl.draw(args.viewport(), |context, gl| {
-            // Clear the screen.
-            clear([0.0, 1.0, 0.0, 1.0], gl);
-
-            for primitive in &primitives {
-                match primitive.kind {
-
-                    PrimitiveKind::Line { ref color, thickness, x1, y1, x2, y2 } => {
-                        line([color[1], color[2], color[3], color[0]],
-                            thickness as f64 / 2.0,
-                            [x1 as f64, y1 as f64, x2 as f64, y2 as f64],
-                            context.transform.trans(400.0,200.0).rot_rad(rotate).trans(-200.0,-100.0), gl);
-                    },
-
-                    PrimitiveKind::Rectangle { ref color, x, y, width, height } => {
-                        rectangle([color[1], color[2], color[3], color[0]],
-                                  [x as f64, y as f64, width as f64, height as f64],
-                                  context.transform.trans(400.0,200.0).rot_rad(rotate).trans(-200.0,-100.0), gl);
-                    },
-
-                    PrimitiveKind::Text { ref color, x, y, size, text: ref src_text } => {
-                        text([color[1], color[2], color[3], color[0]],
-                            size as u32,
-                            src_text,
-                            glyph_cache,
-                            context.transform.trans(400.0,200.0).rot_rad(rotate).trans(x as f64, (y + size) as f64).trans(-200.0,-100.0), gl);
-                    }
-
-                }
-            }
-        });*/
     }
 
-    pub fn text_width(&mut self, size: f32, text: &str) -> f32 {
-        //self.glyph_cache.width(size as FontSize, &text) as f32
-        1.0
+    fn text(&mut self, color: Color, size: u32, src_text: &'static str, matrix: nalgebra::Matrix3<f32>) {
+
+    }
+
+    fn view_matrix_from_resolution(width: f32, height: f32) -> nalgebra::Matrix3<f32> {
+        nalgebra::Matrix3::new(2.0f32 / (width - 1.0f32), 0.0f32, -1.0f32,
+            0.0f32, -2.0f32 / (height - 1.0f32), 1.0f32,
+            0.0f32, 0.0f32, 1.0f32)
     }
 }
