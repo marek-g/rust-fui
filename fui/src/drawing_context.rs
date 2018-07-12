658190b2b065a1eea7bfcc95732356b7387606cf
Marek G <marek@nomail.com>
2018-07-12 20:42:25 +0200

    Updated create_texture api.

--- fui/src/drawing_context.rs
+++ fui/src/drawing_context.rs
@@ -79,7 +79,7 @@ impl DrawingContext {
 
     pub fn create_texture(&mut self, memory: &[u8], width: u16, height: u16, format: ColorFormat, updatable: bool) -> i32 {
         let texture_id = self.resources.get_next_texture_id();
-        let texture = self.renderer.backend().create_texture(memory, width, height, format, updatable);
+        let texture = self.renderer.backend().create_texture(Some(memory), width, height, format, updatable);
         self.resources.textures_mut().insert(texture_id, texture);
         texture_id
     }
