a3264180ff05100faa552ff802075165c2da0a86
Marek G <marek-dev@yandex.com>
2022-01-21 11:22:10 +0100

    Removed unused drawing_context_proxy.rs

--- fui_app/src/async_code/mod.rs
+++ fui_app/src/async_code/mod.rs
@@ -1,11 +1,9 @@
 mod application_async;
 mod channel_dispatcher;
-mod drawing_context_proxy;
 mod window_async;
 mod window_manager_async;
 
 pub use application_async::*;
 pub use channel_dispatcher::*;
-pub use drawing_context_proxy::*;
 pub use window_async::*;
 pub use window_manager_async::*;
