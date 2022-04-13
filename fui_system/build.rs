e4f50d52d6368a9582629c1c58c83e0fa927b1e9
Marek G <marek-dev@yandex.com>
2022-04-13 14:53:29 +0200

    Update build script to MSVC compiler

--- fui_system/build.rs
+++ fui_system/build.rs
@@ -3,13 +3,16 @@ use std::path::Path;
 use std::process::Command;
 
 fn main() {
+    // https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-build-scripts
     let target = env::var("TARGET").unwrap();
     let current_dir = env::current_dir().unwrap();
     let out_dir = env::var("OUT_DIR").unwrap();
+    let env = std::env::var("CARGO_CFG_TARGET_ENV").unwrap();
 
     println!("target: {}", target);
     println!("current_dir: {}", current_dir.to_string_lossy());
     println!("out_dir: {}", out_dir);
+    println!("env: {}", out_dir);
 
     run_cbindgen();
 
@@ -17,10 +20,10 @@ fn main() {
         &current_dir.join("src/platform/qt/qt_wrapper/cpp"),
         &out_dir,
     );
-    run_make(&out_dir);
+    run_make(&env, &out_dir);
 
     cargo_link_static(&out_dir, "qt_wrapper");
-    cargo_link_qt();
+    cargo_link_qt(&env);
 
     generate_bindings("src/platform/qt/qt_wrapper/cpp/qt_wrapper.h", &out_dir);
 }
@@ -50,12 +53,19 @@ fn run_qmake(src_dir: &Path, out_dir: &str) {
     assert!(output.status.success(), "failed to execute qmake process");
 }
 
-fn run_make(dir: &str) {
-    let output = Command::new("make")
-        .args(&["-j16"])
-        .current_dir(&Path::new(&dir))
-        .output()
-        .expect("failed to execute 'make' process");
+fn run_make(env: &str, dir: &str) {
+    let output = if env == "msvc" {
+        Command::new("nmake")
+            .current_dir(&Path::new(&dir))
+            .output()
+            .expect("failed to execute 'make' process")
+    } else {
+        Command::new("make")
+            .args(&["-j16"])
+            .current_dir(&Path::new(&dir))
+            .output()
+            .expect("failed to execute 'make' process")
+    };
 
     println!("make.status: {}", output.status);
     println!("make.stdout: {}", String::from_utf8_lossy(&output.stdout));
@@ -80,7 +90,7 @@ fn qmake_query(var: &str) -> String {
     .expect("UTF-8 conversion failed")
 }
 
-fn cargo_link_qt() {
+fn cargo_link_qt(env: &str) {
     let qt_library_path = qmake_query("QT_INSTALL_LIBS");
 
     println!("cargo:rustc-link-search={}", qt_library_path);
@@ -88,7 +98,9 @@ fn cargo_link_qt() {
     println!("cargo:rustc-link-lib={}", "Qt5Gui");
     println!("cargo:rustc-link-lib={}", "Qt5Core");
 
-    println!("cargo:rustc-link-lib=stdc++");
+    if env != "msvc" {
+        println!("cargo:rustc-link-lib=stdc++");
+    }
 }
 
 fn generate_bindings(src: &str, out_dir: &str) {
